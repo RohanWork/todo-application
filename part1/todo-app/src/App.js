f8451a54af006ffe747d2861511d8c752273506c
RohanWork <rohankatarework@gmail.com>
2022-08-20 14:17:56 +0530

    function components

--- part1/todo-app/src/App.js
+++ part1/todo-app/src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
+import "./App.css";
+// import React from 'react';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      Hello, I am Rohan.
-    </div>
-  );
+    return (
+        <div className="App">
+          Hello, I am Rohan.
+          <FirstComponent />
+        </div>
+    );
+}
+
+function FirstComponent() {
+    return (
+      <div className="FirstComponent">
+        I am creating a todo application using react and springboot
+      </div>
+    );
 }
 
 export default App;
